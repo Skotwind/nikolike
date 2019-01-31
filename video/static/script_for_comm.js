ccd9ea5f02de82d5a19ab8e3bec9884d09fa8f96
skotwind <Skotwind@gmail.com>
2019-01-31 21:32:57 +0300

    -Дохуя всего

---
+++ video/static/script_for_comm.js
@@ -0,0 +1,26 @@
+jQuery("document").ready(function () {
+
+    jQuery(".btn-success").on('click', function ()
+    {
+        var text = jQuery('textarea').val();
+        var idvidos = jQuery('textarea').attr('id');
+        console.log(text);
+        console.log(idvidos);
+        jQuery.ajax({
+            type:"GET",
+            url:"/video/addcommajax/",
+            data:{"text": text, "idvideo":idvidos},
+            contentType: "application/json; charset=utf-8",
+            datatype:"json",
+            catch:false,
+            success: function(data){
+                console.log(data['user']);
+                console.log(data['date']);
+            },
+        })
+    });
+},
+);
+
+
+
